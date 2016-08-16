239b8eef2d8ef3083e2c071e5c7be0002db4c35a
Charles Zhang <zhangchaoxu@gmail.com>
2016-08-16 09:59:22 +0800

    add no page fragment

---
+++ bone/src/main/java/com/idogfooding/bone/ui/recycler/NoPageFragment.java
@@ -0,0 +1,58 @@
+package com.idogfooding.bone.ui.recycler;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.marshalchen.ultimaterecyclerview.quickAdapter.easyRegularAdapter;
+
+import java.util.List;
+
+/**
+ * NoPageFragment
+ *
+ * @author Charles
+ */
+public abstract class NoPageFragment<T, A extends easyRegularAdapter> extends UltimateRecyclerViewFragment<A> {
+
+    @Override
+    protected void afterViewCreated(View view, Bundle savedInstanceState) {
+        super.afterViewCreated(view, savedInstanceState);
+        loadList(true);
+    }
+
+    protected void loadList(final boolean refresh) {
+
+    }
+
+    @Override
+    protected void refresh() {
+        loadList(true);
+    }
+
+    @Override
+    protected boolean getIsLoadMore() {
+        return false;
+    }
+
+    protected void loadListOnSubscribe() {
+        ultimateRecyclerView.setRefreshing(true);
+    }
+
+    protected void loadListOnNext(boolean refresh, List<T> list) {
+        if (refresh) {
+            adapter.removeAll();
+        }
+        if (list.isEmpty()) {
+            ultimateRecyclerView.showEmptyView();
+        } else {
+            adapter.insert(list);
+        }
+        ultimateRecyclerView.setRefreshing(false);
+    }
+
+    protected void loadListOnError(Throwable throwable) {
+        ultimateRecyclerView.setRefreshing(false);
+        handleNetworkError(throwable);
+    }
+
+}
