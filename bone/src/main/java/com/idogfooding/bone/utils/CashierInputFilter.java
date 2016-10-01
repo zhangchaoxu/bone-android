723591df2b5b31202a9339dd8a0e64d0add9c29e
Charles <zhangchaoxu@gmail.com>
2016-10-01 18:09:17 +0800

    加入金钱限制

--- bone/src/main/java/com/idogfooding/bone/utils/CashierInputFilter.java
+++ bone/src/main/java/com/idogfooding/bone/utils/CashierInputFilter.java
@@ -13,36 +13,37 @@ import java.util.regex.Pattern;
  * @author Charles
  */
 public class CashierInputFilter implements InputFilter {
-    private Pattern mPattern;
-    //输入的最大金额
-    private final int MAX_VALUE = Integer.MAX_VALUE;
 
-    //小数点后的2位数
-    private final int POINTER_AFTER_LENGTH = 1;
+    Pattern mPattern;
 
-    //小数点前的7位数
-    private int POINTER_BEFORE_LENGTH = 1;
+    // 输入的最大金额
+    private double maxValue = 9.9d;
+    // 小数点后的位数
+    private int pointerLength = 1;
 
-    private final String POINTER = ".";
+    private static final String POINTER = ".";
 
     private static final String ZERO = "0";
 
-    public CashierInputFilter() {
+    public CashierInputFilter(int pointerLength, double maxValue) {
         mPattern = Pattern.compile("([0-9]|\\.)*");
-    }
-
-    public CashierInputFilter(int _maxLength) {
-        mPattern = Pattern.compile("([0-9]|\\.)*");
-        POINTER_BEFORE_LENGTH = _maxLength - POINTER_AFTER_LENGTH - 1;
+        this.pointerLength = pointerLength;
+        this.maxValue = maxValue;
     }
 
     /**
-     * @param source 新输入的字符串
-     * @param start  新输入的字符串起始下标，一般为0
-     * @param end    新输入的字符串终点下标，一般为source长度-1
-     * @param dest   输入之前文本框内容
-     * @param dstart 原内容起始坐标，一般为0
-     * @param dend   原内容终点坐标，一般为dest长度-1
+     * @param source
+     *            新输入的字符串
+     * @param start
+     *            新输入的字符串起始下标，一般为0
+     * @param end
+     *            新输入的字符串终点下标，一般为source长度-1
+     * @param dest
+     *            输入之前文本框内容
+     * @param dstart
+     *            原内容起始坐标，一般为0
+     * @param dend
+     *            原内容终点坐标，一般为dest长度-1
      * @return 输入内容
      */
     @Override
@@ -50,58 +51,47 @@ public class CashierInputFilter implements InputFilter {
         String sourceText = source.toString();
         String destText = dest.toString();
 
-        //验证删除等按键
+        // 验证删除等按键
         if (TextUtils.isEmpty(sourceText)) {
             return "";
         }
 
         Matcher matcher = mPattern.matcher(source);
-        //已经输入小数点的情况下，只能输入数字
+        // 已经输入小数点的情况下，只能输入数字
         if (destText.contains(POINTER)) {
             if (!matcher.matches()) {
                 return "";
             } else {
-                if (POINTER.equals(source)) {  //只能输入一个小数点
+                if (POINTER.equals(source)) { // 只能输入一个小数点
                     return "";
                 }
             }
 
+            // 验证小数点精度，保证小数点后只能输入两位
             int index = destText.indexOf(POINTER);
-            //验证小数点精度，保证小数点后只能输入两位
-            int afterLength = dend - index;
+            int length = dend - index;
 
-            // 输入后，修改控制不了，//验证小数点精度，保证小数点前位数
-
-            if (dstart == POINTER_BEFORE_LENGTH && index == POINTER_BEFORE_LENGTH) {
-                return dest.subSequence(dstart, dend);
-            }
-
-            //验证小数点精度，保证小数点后只能输入两位
-            if (afterLength > POINTER_AFTER_LENGTH) {
+            if (length > pointerLength) {
                 return dest.subSequence(dstart, dend);
             }
         } else {
-            //没有输入小数点的情况下，只能输入小数点和数字，但首位不能输入小数点和0
+            // 没有输入小数点的情况下，只能输入小数点和数字，但首位不能输入小数点和0
             if (!matcher.matches()) {
                 return "";
             } else {
-                //验证小数点精度，保证小数点前只能输入7位
-
-                if (dstart == POINTER_BEFORE_LENGTH) {
-                    if (sourceText.contains(POINTER))
-                        return dest.subSequence(dstart, dend) + sourceText + ZERO + ZERO;
-                    else
-                        return dest.subSequence(dstart, dend) + POINTER + ZERO + ZERO;
+                //if ((POINTER.equals(source) || ZERO.equals(source)) && TextUtils.isEmpty(destText)) {
+                if (POINTER.equals(source) && TextUtils.isEmpty(destText)) {
+                    return "";
                 }
-                if ((POINTER.equals(source) || ZERO.equals(source)) && TextUtils.isEmpty(destText)) {
-                    return ZERO + POINTER + ZERO + ZERO;
+                if (ZERO.equals(source) && ZERO.equals(destText)) {
+                    return "";
                 }
             }
         }
 
-        //验证输入金额的大小
+        // 验证输入金额的大小
         double sumText = Double.parseDouble(destText + sourceText);
-        if (sumText >= MAX_VALUE) {
+        if (sumText > maxValue) {
             return dest.subSequence(dstart, dend);
         }
 
