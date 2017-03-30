2b5488524c1d947dbf86f3c2579577f60d50747e
Charles <zhangchaoxu@gmail.com>
2017-03-30 16:34:35 +0800

    add tbs

--- TBS/src/main/java/com/idogfooding/tbs/BrowserActivity.java
+++ TBS/src/main/java/com/idogfooding/tbs/BrowserActivity.java
@@ -1,6 +1,10 @@
 package com.idogfooding.tbs;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebView;
 
 /**
  * BrowserActivity
@@ -10,5 +14,20 @@ import android.support.v7.app.AppCompatActivity;
 public class BrowserActivity extends AppCompatActivity {
 
 
+    WebView wvBrowser;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.browser);
+
+        wvBrowser = (WebView) findViewById(R.id.wv_browser);
+    }
+
+    @Override
+    protected void onDestroy() {
+        wvBrowser.setVisibility(View.GONE);
+        wvBrowser.destroy();
+        super.onDestroy();
+    }
 }
