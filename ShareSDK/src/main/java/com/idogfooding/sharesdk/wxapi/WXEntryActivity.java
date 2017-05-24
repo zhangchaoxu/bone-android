13cf410f730311e931053680629a91d7b7979dbe
Charles <zhangchaoxu@gmail.com>
2017-05-24 16:33:48 +0800

    upgrade sharesdk

--- ShareSDK/src/main/java/com/idogfooding/sharesdk/wxapi/WXEntryActivity.java
+++ ShareSDK/src/main/java/com/idogfooding/sharesdk/wxapi/WXEntryActivity.java
@@ -1,16 +1,15 @@
 /*
- * copy below class to wxapi folder under your app package
+ * 官网地站:http://www.mob.com
+ * 技术支持QQ: 4006852216
+ * 官方微信:ShareSDK   （如果发布新版本的话，我们将会第一时间通过微信将版本更新内容推送给您。如果使用过程中有任何问题，也可以通过微信与我们取得联系，我们将会在24小时内给予回复）
+ *
+ * Copyright (c) 2013年 mob.com. All rights reserved.
  */
-/*package com.idogfooding.sharesdk.wxapi;
 
-import android.content.Intent;
-import android.widget.Toast;
-import cn.sharesdk.wechat.utils.WXAppExtendObject;
-import cn.sharesdk.wechat.utils.WXMediaMessage;
-import cn.sharesdk.wechat.utils.WechatHandlerActivity;
+package com.idogfooding.sharesdk.wxapi;
 
-*//** 微信客户端回调activity示例 *//*
-public class WXEntryActivity extends WechatHandlerActivity {
+/** 微信客户端回调activity示例 */
+/*public class WXEntryActivity extends WechatHandlerActivity {
 
 	*//**
 	 * 处理微信发出的向第三方应用请求app message
