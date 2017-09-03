1914ebc137681b4599cc6871ddee6a494aed0a6e
Charles <zhangchaoxu@gmail.com>
2017-09-03 08:13:13 +0800

    upgrade tbs version

--- TBS/src/main/java/com/idogfooding/tbs/utils/X5WebView.java
+++ TBS/src/main/java/com/idogfooding/tbs/utils/X5WebView.java
@@ -3,20 +3,16 @@ package com.idogfooding.tbs.utils;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.TextView;
 
-import com.tencent.smtt.sdk.QbSdk;
 import com.tencent.smtt.sdk.WebSettings;
 import com.tencent.smtt.sdk.WebSettings.LayoutAlgorithm;
 import com.tencent.smtt.sdk.WebView;
 import com.tencent.smtt.sdk.WebViewClient;
 
 public class X5WebView extends WebView {
-    TextView title;
+
     private WebViewClient client = new WebViewClient() {
         /**
          * 防止加载网页时调起系统浏览器
@@ -57,34 +53,11 @@ public class X5WebView extends WebView {
         webSetting.setPluginState(WebSettings.PluginState.ON_DEMAND);
         // webSetting.setRenderPriority(WebSettings.RenderPriority.HIGH);
         webSetting.setCacheMode(WebSettings.LOAD_NO_CACHE);
-
-        // this.getSettingsExtension().setPageCacheCapacity(IX5WebSettings.DEFAULT_CACHE_CAPACITY);//extension
-        // settings 的设计
     }
 
     @Override
     protected boolean drawChild(Canvas canvas, View child, long drawingTime) {
-        boolean ret = super.drawChild(canvas, child, drawingTime);
-        canvas.save();
-        Paint paint = new Paint();
-        paint.setColor(0x7fff0000);
-        paint.setTextSize(24.f);
-        paint.setAntiAlias(true);
-        if (getX5WebViewExtension() != null) {
-            canvas.drawText(this.getContext().getPackageName() + "-pid:"
-                    + android.os.Process.myPid(), 10, 50, paint);
-            canvas.drawText(
-                    "X5  Core:" + QbSdk.getTbsVersion(this.getContext()), 10,
-                    100, paint);
-        } else {
-            canvas.drawText(this.getContext().getPackageName() + "-pid:"
-                    + android.os.Process.myPid(), 10, 50, paint);
-            canvas.drawText("Sys Core", 10, 100, paint);
-        }
-        canvas.drawText(Build.MANUFACTURER, 10, 150, paint);
-        canvas.drawText(Build.MODEL, 10, 200, paint);
-        canvas.restore();
-        return ret;
+        return super.drawChild(canvas, child, drawingTime);
     }
 
     public X5WebView(Context arg0) {
