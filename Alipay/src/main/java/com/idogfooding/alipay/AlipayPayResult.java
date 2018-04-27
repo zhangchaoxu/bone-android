4c1879b09b2fe738c575703caef412c1df1b642d
Charles <zhangchaoxu@gmail.com>
2018-04-27 13:20:56 +0800

    upgrade alipay

---
+++ Alipay/src/main/java/com/idogfooding/alipay/AlipayPayResult.java
@@ -0,0 +1,62 @@
+package com.idogfooding.alipay;
+
+import android.text.TextUtils;
+
+import java.io.Serializable;
+import java.util.Map;
+
+/**
+ * 支付宝支付结果
+ *
+ * @author Charles
+ */
+public class AlipayPayResult implements Serializable {
+
+    private String resultStatus;
+    private String result;
+    private String memo;
+
+    public AlipayPayResult(Map<String, String> rawResult) {
+        if (rawResult == null) {
+            return;
+        }
+
+        for (String key : rawResult.keySet()) {
+            if (TextUtils.equals(key, "resultStatus")) {
+                resultStatus = rawResult.get(key);
+            } else if (TextUtils.equals(key, "result")) {
+                result = rawResult.get(key);
+            } else if (TextUtils.equals(key, "memo")) {
+                memo = rawResult.get(key);
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "resultStatus={" + resultStatus + "};memo={" + memo
+                + "};result={" + result + "}";
+    }
+
+    /**
+     * @return the resultStatus
+     */
+    public String getResultStatus() {
+        return resultStatus;
+    }
+
+    /**
+     * @return the memo
+     */
+    public String getMemo() {
+        return memo;
+    }
+
+    /**
+     * @return the result
+     */
+    public String getResult() {
+        return result;
+    }
+
+}
---
+++ Alipay/src/main/java/com/idogfooding/alipay/Const.java
@@ -0,0 +1,6 @@
+package com.idogfooding.alipay;
+
+public class Const {
+    public static final int ALIPAY_SDK_PAY_FLAG = 1;
+    public static final int ALIPAY_SDK_AUTH_FLAG = 2;
+}
